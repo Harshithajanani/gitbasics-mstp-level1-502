673576169b6528f54cfebf274ac42ac8005d0c7a
Harshithajanani <harshitagowri10@gmail.com>
2019-10-01 18:09:52 +0530

    watching

---
+++ 01-10-2019/packages/operations.py
@@ -0,0 +1,16 @@
+def addition(a,b):
+    c=a+b
+    print(c)
+def primeornot(n):
+    fact=0
+    for i in range(1,n+1):
+        if(n%i==0):
+            fact=fact+1
+    if(fact==2):
+        return True
+    else:
+        return False
+
+    
+
+    
